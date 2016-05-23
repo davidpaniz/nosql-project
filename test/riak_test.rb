39fd55516becd9345514dfdf65a130dfb59ff0b3
David Paniz <davidpaniz@gmail.com>
2016-05-23 15:00:38 -0300

    Mais testes de riak

--- test/riak_test.rb
+++ test/riak_test.rb
@@ -3,16 +3,36 @@ require 'riak'
 
 class TestRiak < Minitest::Test
   def setup
-    @riak  = Riak::Client.new
-    @votacao = @riak.bucket 'votacao'
+    @riak  = Riak::Client.new()
+    @votacao = @riak.bucket "votacao#{rand()}"
+  end
+
+  def cria_riak_object(chave, valor)
+    voto = Riak::RObject.new(@votacao, chave)
+    voto.content_type = "text/plain"
+    voto.raw_data = valor
+    voto
   end
 
   def test_all
-    voto = Riak::RObject.new(@votacao, "davidpaniz")
-    voto.content_type = "text/plain"
-    voto.raw_data = "Fulano"
-    voto.store
+    assert_equal [], @riak.list_keys(@votacao)
 
+    #adiciona primeiro voto
+    voto = cria_riak_object "davidpaniz", "Fulano"
+    voto.store
     assert_equal "Fulano", @votacao.get("davidpaniz").data
+
+    assert_equal ["davidpaniz"], @riak.list_keys(@votacao)
+
+    # adiciona segundo voto
+    voto = cria_riak_object "adriano", "Ciclano"
+    voto.store
+    assert_equal "Ciclano", @votacao.get("adriano").data
+
+    assert_includes @riak.list_keys(@votacao), "adriano", "davidpaniz"
+
+    #removendo primeiro voto
+    @votacao.delete("davidpaniz")
+    refute @votacao.exists?("davidpaniz")
   end
 end
