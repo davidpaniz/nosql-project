7e6d49cab3805bdbd79d8506a0d58785265ee178
David Paniz <davidpaniz@gmail.com>
2016-05-23 17:25:44 -0300

    Usando counter no riak

--- test/riak_test.rb
+++ test/riak_test.rb
@@ -3,21 +3,24 @@ require 'riak'
 
 class TestRiak < Minitest::Test
   def setup
-    @riak  = Riak::Client.new()
-    @votacao = @riak.bucket "votacao#{rand()}"
+    @riak                = Riak::Client.new()
+    @votacao             = @riak.bucket "votacao#{rand(999999)}"
+    @contador            = @riak.bucket "contador#{rand(999999)}"
+    @contador.props      = { "datatype" => "counter" }
+    @contador.allow_mult = true
   end
 
   def cria_riak_object(chave, valor)
-    voto = Riak::RObject.new(@votacao, chave)
+    voto              = Riak::RObject.new(@votacao, chave)
     voto.content_type = "text/plain"
-    voto.raw_data = valor
+    voto.raw_data     = valor
     voto
   end
 
   def test_all
     assert_equal [], @riak.list_keys(@votacao)
 
-    #adiciona primeiro voto
+    # adiciona primeiro voto
     voto = cria_riak_object "davidpaniz", "Fulano"
     voto.store
     assert_equal "Fulano", @votacao.get("davidpaniz").data
@@ -31,8 +34,8 @@ class TestRiak < Minitest::Test
 
     assert_includes @riak.list_keys(@votacao), "adriano", "davidpaniz"
 
-    #update
-    voto = @votacao.get("davidpaniz")
+    # update
+    voto          = @votacao.get("davidpaniz")
     voto.raw_data = "Ciclano"
     voto.store
     assert_equal "Ciclano", @votacao.get("davidpaniz").data
@@ -40,5 +43,10 @@ class TestRiak < Minitest::Test
     #removendo primeiro voto
     @votacao.delete("davidpaniz")
     refute @votacao.exists?("davidpaniz")
+
+    # usando counter
+    candidatoX = @contador.counter("candidatoX")
+    candidatoX.increment
+    assert_equal 1, candidatoX.value
   end
 end
